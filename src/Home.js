d8b29811371ea840426ab7eb99c6811183cad63b
doctork4 <doctor.k1994@gmail.com>
2023-07-23 19:21:34 +0900

    Feat::버튼 컴포넌트 핸들러 추가

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,18 @@
+import { useState } from 'react';
 import { Button } from './component/Button';
 
 export default function Home () {
+  const [ count, setCount ] = useState(0);
+  
+  const handleClick = () => {
+    setCount(count => count+1);
+  };
+
   return (
     <div>
       <p>Home page 입니다.</p>
-      <Button type={'button'} disabled={false}>Click</Button>
+      <Button type={'button'} disabled={false} onClick={()=>handleClick()}>Click</Button>
+      <p>{count}</p>
     </div>
   );
 };
--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -1,8 +1,9 @@
-export const Button = ({children, type, disabled}) => {
+export const Button = ({children, type, disabled, onClick}) => {
   return (
     <button
     type={type}
     disabled={disabled}
+    onClick={onClick}
     >
       {children}
     </button>
